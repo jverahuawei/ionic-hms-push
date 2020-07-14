faa679c47850fd3ed06b21c267edc37664f014f2
jose vera <jose.vera@huawei.com>
2020-07-13 21:49:46 -0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,105 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var cordova: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
+export class HomePage  implements OnInit{
+  pushLog: string = "";
   constructor() {}
+
+  ngOnInit() {
+    this.setMessageCallback();
+  }
+
+  getToken() {
+    console.log("GetToken");
+    try {
+      cordova.plugins.CordovaHMSPushPlugin.getToken(
+        "index.js",
+        (_res) => {
+          this.pushLog =
+            this.pushLog + this.appendP("Get Token Successfully", "success");
+
+          this.pushLog =
+            this.pushLog + this.appendP("Push Token : " + _res, "normal");
+
+          this.updateLog();
+        },
+        (_err) => {
+          this.pushLog =
+            this.pushLog + this.appendP("Get Token Fail: " + _err, "error");
+          this.updateLog();
+        }
+      );
+    } catch (_e) {
+      alert(JSON.stringify(_e, null, 4));
+    }
+  }
+
+  topicSubscribe(topic) {
+    console.log("TopicSubscribe");
+    try {
+      cordova.plugins.CordovaHMSPushPlugin.subscribeTopic(
+        topic,
+        (_res) => {
+          this.pushLog =
+            this.pushLog +
+            this.appendP("Topic subcription Successfully", "success");
+
+          this.pushLog = this.pushLog + this.appendP(_res, "normal");
+
+          this.updateLog();
+        },
+        (_err) => {
+          this.pushLog =
+            this.pushLog +
+            this.appendP("Topic subcription Fail:" + _err, "error");
+          this.updateLog();
+        }
+      );
+    } catch (_e) {
+      alert("error");
+      alert(JSON.stringify(_e, null, 4));
+    }
+  }
+
+  setMessageCallback() {
+    console.log("SetMessageCallback");
+    try {
+      cordova.plugins.CordovaHMSPushPlugin.getMessageCallback(
+        "index.js",
+        (_res) => {
+          this.pushLog =
+            this.pushLog + this.appendP("Data Message Callback", "heading");
+
+          this.pushLog = this.pushLog + this.appendP(_res, "heading");
+          this.updateLog();
+        },
+        (_err) => {
+          this.pushLog = this.pushLog + this.appendP(_err, "error");
+          this.updateLog();
+        }
+      );
+    } catch (_e) {
+      alert(JSON.stringify(_e, null, 4));
+    }
+  }
+
+  updateLog() {
+    document.getElementById("pushLog").innerHTML = this.pushLog;
+  }
+
+  clearLog() {
+    this.pushLog = "";
+    this.updateLog();
+  }
+
+  appendP(text: String, pClass: String) {
+    return "<p class='" + pClass + "'>" + text + "</p>";
+  }
+
 
 }
